a5f51cae6877b6331727bda16bf62e5f1a2758a0
Ben <benc269@gmail.com>
2019-08-18 20:49:04 +0300

    Basic login titles and logo

--- tzvi-talk/src/App.js
+++ tzvi-talk/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import SignInBlock from './Components/SignInBlockComponent/SignInBlock';
+
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="logo" className="logo" />
       <p className="main-title">Welcome To Tzvi Talk!</p>
       <p className="sub-title">Sign In To Continue.</p>
+      <SignInBlock />
     </div>
   );
 }
---
+++ tzvi-talk/src/Components/SignInBlockComponent/SignInBlock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SignInBlock.css';
+
+const SignInBlock = () => {
+  return (
+    <div className="sign-in-block">
+      <p>username:</p>
+    </div>
+  );
+}
+
+export default SignInBlock;
