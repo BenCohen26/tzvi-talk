d6735a393ccd4d82f014fd6f122135f0766c96a1
Ben <benc269@gmail.com>
2019-08-18 20:31:43 +0300

    created basic welcome screen

---
+++ tzvi-talk/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="main-title">Welcome To Tzvi Talk!</p>
+      <p className="sub-title">Sign In To Continue.</p>
+    </div>
+  );
+}
+
+export default App;
